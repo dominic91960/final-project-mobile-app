14a476b860a657bec55c1924d3080788b7c58a71
Dominic Brian <dominic91960@gmail.com>
2024-05-29 11:34:34 +0530

    Third Commit: Removed unwanted files.

--- app/(tabs)/assistant.jsx
+++ app/(tabs)/assistant.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const SignIn = () => {
+const Create = () => {
   return (
     <View>
-      <Text>SignIn</Text>
+      <Text>Create</Text>
     </View>
   );
 };
 
-export default SignIn;
+export default Create;
