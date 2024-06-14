006344ca63341ba58775fe638602ac77a26e0bca
GeenethM <geenethmedagedara@gmail.com>
2024-06-14 18:43:27 +0530

    test one

--- app/(tabs)/assistant.jsx
+++ app/(tabs)/assistant.jsx
@@ -1,12 +1,20 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 const Assistant = () => {
   return (
-    <View>
-      <Text>Assistant</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Chatbot />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
 
 export default Assistant;
